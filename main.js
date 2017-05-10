3cf0eec2c198c66145c5df1c7038e6633540c808
Kryštof Rainisch <rainikry@fel.cvut.cz>
2017-05-10 23:02:36 +0200

    randomness

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 'use strict'
-const DIAMETER = 12
+const DIAMETER = 12,
+      MAX_ELEVATION = 100,
+      RANDOMNESS = .5
 
 function main() {
   var canvas = document.getElementById('canvas')
   var ctx = canvas.getContext('2d')
   var map = new Map(DIAMETER, ctx)
+
+  map.randomHexagon().elevation = MAX_ELEVATION
+
+  for (var i = 0; i < 10000; i++) {
+    map.elevate(map.randomHexagon())
+  }
   map.draw(25)
   return map;
 }
 
+function shuffle(array) {
+  var a = array.slice()
+  for (let i = a.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+  return a;
+}
+
+
 class Hexagon {
-  constructor(x,y,z, context) {
+  constructor(x,y,z, context, parent) {
     this.x = x
     this.y = y
     this.z = z
+    this.elevation = null
     this.context = context
+    this.parent = parent
   }
 
   draw(size){
     this.context.save()
     this.context.translate(this.context.canvas.clientWidth/2, this.context.canvas.clientHeight/2)
 
+
     // Z axis
-    this.context.translate(this.z*(size/6)*5, this.z*(size*1.5))
+    this.context.translate(this.z*(size*(5/6)), this.z*(size*1.5))
     // X axis
     this.context.translate(-this.x*(size/12)*11, this.x*(1.5*size))
     // draw hexagon
@@ -34,7 +55,6 @@ class Hexagon {
   drawPixels(h){
     var size = h
     var angle_deg, angle_rad
-
     this.context.save()
 
     this.context.beginPath()
@@ -61,15 +81,20 @@ class Hexagon {
 
     this.context.fillStyle = 'rgb(' + red + ', ' + blue + ', ' + green + ')'
 
+    if (this.elevation != null) {
+      var shade = 255-Math.floor(255/MAX_ELEVATION*this.elevation)
+      this.context.fillStyle = 'rgb(' + shade + ', ' + shade + ', ' + shade + ')'
+    }
+
 
     this.context.fill()
     this.context.restore()
 
-    this.context.fillStyle = 'white'
-    this.context.font = '10px Courier';
-    this.context.fillText(''+this.x, size-6, (size/3)*2+10);
-    this.context.fillText(''+this.y, -(size/3)*2, size/2);
-    this.context.fillText(''+this.z, (size/3)*2-12, size/2);
+    // this.context.fillStyle = 'white'
+    // this.context.font = '10px Courier';
+    // this.context.fillText(''+this.x, size-6, (size/3)*2+10);
+    // this.context.fillText(''+this.y, -(size/3)*2, size/2);
+    // this.context.fillText(''+this.z, (size/3)*2-12, size/2);
   }
 }
 
@@ -78,6 +103,7 @@ class Map {
     this.context = context
     this.size = size
     this.hexagons = {}
+    this.keys = []
     this.populate()
   }
 
@@ -86,10 +112,8 @@ class Map {
       for (var y = -this.size; y <= this.size; y++) {
         for (var z = -this.size; z <= this.size; z++) {
           if ( (x+y+z) == 0 ) {
-            if(x==1 && y==9 && z==-10){
-              debugger
-            }
-            this.hexagons[this.hash(x,y,z)] = new Hexagon(x,y,z, this.context)
+            this.hexagons[this.hash(x,y,z)] = new Hexagon(x,y,z, this.context, this)
+            this.keys.push(this.hash(x,y,z))
           }
         }
       }
@@ -99,9 +123,25 @@ class Map {
   hash(x,y,z){
     return ("hex_"+(1000-x)+(1000-y)+(1000-z))
   }
+
   getHexagon(x,y,z){
-    return this.hexagons[this.hash(x,y,z)]
+    if (this.hexagons[this.hash(x,y,z)]) {
+      return this.hexagons[this.hash(x,y,z)]
+    } else return null
   }
+
+  randomHexagon(){
+    var z = Math.floor(Math.random()*(DIAMETER*2))-DIAMETER
+    var x = DIAMETER - Math.floor(Math.random()*( DIAMETER*2-Math.abs(z) ))
+    var y = - x - z;
+
+    var hex = this.getHexagon(x,y,z);
+
+    if ((x+y+z) == 0 && hex == null){
+      return this.randomHexagon()
+    } else return hex
+  }
+
   draw(width){
     for (var key in this.hexagons) {
       if (this.hexagons.hasOwnProperty(key)) {
@@ -109,6 +149,68 @@ class Map {
       }
     }
   }
+  getNeigbours(parent){
+    var x = parent.x;
+    var y = parent.y;
+    var z = parent.z;
+    var result = []
+    var hex;
+
+    var hex = this.getHexagon( x+1, y, z-1 );
+    if( hex != null) result.push(hex)
+
+    var hex = this.getHexagon( x-1, y, z+1 );
+    if( hex != null) result.push(hex)
+
+    var hex = this.getHexagon( x, y+1, z-1 );
+    if( hex != null) result.push(hex)
+
+    var hex = this.getHexagon( x, y-1, z-1 );
+    if( hex != null) result.push(hex)
+
+    var hex = this.getHexagon( x+1, y-1, z );
+    if( hex != null) result.push(hex)
+
+    var hex = this.getHexagon( x-1, y+1, z );
+    if( hex != null) result.push(hex)
+
+    return result;
+  }
+
+  elevate(hex){
+    var neighbours = [];
+    var elevation = null;
+    var result = false;
+    if (hex.elevation == null) {
+      neighbours = this.getNeigbours(hex)
+
+      if(neighbours != []){
+        for (var i = 0; i < neighbours.length; i++) {
+          if (elevation == null && neighbours[i].elevation != null) {
+            elevation = Math.floor(neighbours[i].elevation*.9 + Math.random()*neighbours[i].elevation*.1)
+          } else if (neighbours[i].elevation != null && neighbours[i].elevation < elevation*.9 ){
+            elevation = Math.floor(Math.random()*neighbours[i].elevation*.9)
+          }
+        }
+        if (elevation != null) {
+          hex.elevation = elevation
+          console.log(elevation);
+
+          neighbours = neighbours.filter(function(neighbour){
+            return neighbours.elevation == null;
+          })
+
+          if(neighbours != [] && Math.random() > RANDOMNESS){
+            this.elevate(neighbours[Math.floor(Math.random()*(neighbours.length-1))])
+          }
+          result = true
+        }
+
+      }
+    }
+    return result
+  }
+
 }
 
 var m = main()
